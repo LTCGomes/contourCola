04b8fe52bc27e653d322aa0c17b205a723284873
Tiago Santos <tjsantos@ua.pt>
2018-10-23 16:05:45 +0100

    classes principais para a biblioteca criadas

---
+++ Cola Library/src/Library/contourCola.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Library;
+
+/**
+ *
+ * @author tjsantos
+ */
+public class contourCola {
+
+    public contourCola(String nomeAplicacao, String versao) {
+        //TODO
+    }
+    
+    public boolean isRegister() {
+        //TODO
+        return false;
+    }
+    
+    public boolean startRegistration() {
+        //TODO
+        return false;
+    }
+    
+    public void showLicenseInfo() {
+        //TODO
+    }
+}
